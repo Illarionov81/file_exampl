57ffa3fb851c8bd7fd94160228e045d17a103ce3
alex <nuvorish81@mail.ru>
2021-01-20 19:39:32 +0600

    Добавленно прилажение аккаунтс

---
+++ source/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from accounts.views import RegisterView, UserDetailView, UserChangeView,\
+    UserPasswordChangeView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    # path('login/', LoginViewSession.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail'),
+    # path('users/', AllUserView.as_view(), name='users'),
+    path('<int:pk>/change/', UserChangeView.as_view(), name='change'),
+    path('<int:pk>/password_change/', UserPasswordChangeView.as_view(), name='password_change')
+]
