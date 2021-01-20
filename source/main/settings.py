b76b41a47dc65a535e840c98995f2fffda9958d1
alex <nuvorish81@mail.ru>
2021-01-20 20:04:28 +0600

    Подключен accounts

--- source/main/settings.py
+++ source/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'webapp:photos'
+LOGOUT_REDIRECT_URL = 'webapp:photos'
+LOGIN_URL = 'accounts:login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/media/'
